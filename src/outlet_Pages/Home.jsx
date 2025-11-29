ab0663e7cf6aab03e835b7a0f8b79dad3de99b2c
nafiz93 <nafiz93@gmail.com>
2025-11-29 17:14:14 +0100

    set the catagory card

--- src/outlet_Pages/Home.jsx
+++ src/outlet_Pages/Home.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import bg_image from "../assets/images/dog bg.png";
 
 const Home = () => {
+
+  const tatalCard=4;
   return (
     <div
       style={{ backgroundImage: `url(${bg_image})` }}
       className="h-screen w-full bg-cover bg-center bg-no-repeat"
     >
         <div className="absolute h-full inset-0 bg-black/20"></div>
+
+
+
       
       
     </div>
